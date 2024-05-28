18bce66ad455b41a815679ad635bb91a62fd542a
sajid15511 <133560337+sajid15511@users.noreply.github.com>
2024-05-28 21:55:42 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Book Store MERN stack project
+I am Sajid From MNIT jaipur. I have done my B.Tech in ECE from MNIT jaipur graduate in 2024.
---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://sajid:sajid123@bookstore.xyfontp.mongodb.net/";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
